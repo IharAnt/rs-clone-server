5cf1fc46d43e71afd8bfe3619089d1b17b516ca6
Igor Antonenko <i_antonenko@lesta.group>
2023-02-05 18:51:16 +0300

    feat: add mongodb mongoose

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const start = async () => {
   try {
+    mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     })
