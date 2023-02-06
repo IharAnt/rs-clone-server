66f41bfe16752c8d1a6e1fe554c5987ec3a681ba
Igor Antonenko <i_antonenko@lesta.group>
2023-02-06 12:09:27 +0300

    feat: implement registration method

---
+++ service/TokenService.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const TokenModel = require('../models/TokenModel');
+
+class TokenService {
+  generateToken(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, {
+      expiresIn: '60m',
+    });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN, {
+      expiresIn: '20d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  async saveToken(userId, refreshToken) {
+    const token = await TokenModel.findOne({ user: userId });
+    if(token) {
+      token.refreshToken = refreshToken;
+      return token.save();
+    }
+    const newToken = await TokenModel.create({user: userId, refreshToken});
+    return newToken;
+  }
+}
+
+module.exports = new TokenService();
---
+++ service/UserService.js
@@ -0,0 +1,39 @@
+const UserModel = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./MailService');
+const tokenService = require('./TokenService');
+const UserDto = require('../dtos/UserDto');
+
+class UserService {
+  async registration(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      throw new Error(`User ${email} already exists`);
+    }
+    const hashedPassword = await bcrypt.hash(password, 7);
+    // const activationLInk = uuid.v4();
+    const newUser = await UserModel.create({ email, password: hashedPassword });
+    // await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLInk}`);
+    
+    const userDto = new UserDto(newUser);
+    const tokens = tokenService.generateToken({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    
+    return {
+      ...tokens,
+      user: userDto,
+    };
+  }
+
+  async activate(link) {
+    const user = await UserModel.findOne({ activationLink: link});
+    if (!user) {
+      throw new Error('Activation link is invalid');
+    }
+    user.isActivated = true;
+    user.save();
+  }
+}
+
+module.exports = new UserService();
